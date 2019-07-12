09e95ff2aecb209de0a724d475a3ec15f732732d
Tung Nguyen <tung18tht@gmail.com>
2019-07-12 04:13:18 +0200

    Add MenuScene entry animation

--- MenuScene.js
+++ MenuScene.js
@@ -8,10 +8,16 @@ class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(gameState.width / 2, 150, "PONG", { fontSize: 120, color: '#FFFFFF' }).setOrigin(0.5, 0.5);
+    var title = this.add.text(gameState.width / 2, 150, "PONG", { fontSize: 120, color: '#FFFFFF' }).setOrigin(0.5, 0.5).setAlpha(0);
 
-    var playButton = this.add.rectangle(gameState.width / 2, gameState.height / 2, 150, 150).setOrigin(0.5, 0.5).setStrokeStyle(10, 0xFFFFFF).setInteractive();
-    this.add.image(gameState.width / 2, gameState.height / 2, 'play').setOrigin(0.5, 0.5).setDisplaySize(100, 100);
+    var playButton = this.add.rectangle(gameState.width / 2, gameState.height / 2, 150, 150).setOrigin(0.5, 0.5).setStrokeStyle(10, 0xFFFFFF).setInteractive().setAlpha(0);
+    var playIcon = this.add.image(gameState.width / 2, gameState.height / 2, 'play').setOrigin(0.5, 0.5).setDisplaySize(100, 100).setAlpha(0);
+
+    this.tweens.add({
+      targets: [title, playButton, playIcon],
+      alpha: 1,
+      duration: 500
+    });
 
     var blackCover = this.add.rectangle(0, 0, gameState.width, gameState.height, 0x000000, 0).setOrigin(0, 0);
 
